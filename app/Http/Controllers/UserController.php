d07120cc61c6a9e217546272aea7a6c821cb5019
shofa yasmin fauziah <yasminshofa.sy@gmail.com>
2024-03-15 12:00:46 +0700

    Practicum-2.1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,16 +9,9 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index(){
-        //tambah data user dengan Eloquent Model
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data);
-    
-        $user = UserModel::all();
+        $user = UserModel::findOr(20, ['username','nama'], function(){
+            abort(404);
+        });
         return view('user', ['data' =>$user]);
     }
 }
