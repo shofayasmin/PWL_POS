5d0233a1266f22177829987478a40184a0c1bc1d
shofa yasmin fauziah <yasminshofa.sy@gmail.com>
2024-03-16 20:22:12 +0700

    Practicum-2.5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,112 +6,76 @@ use App\Models\UserModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
+//Lab Course 2.5 – Attribute Changes
 //2.
 // class UserController extends Controller
 // {
 //     public function index()
 //     {
-//         $user = UserModel::firstOrCreate(
+//         $user = UserModel::create(
 //             [
-//                 'username' => 'manager',
-//                 'nama' => 'Manager',
-//             ],
-//         );
-//         return view('user', ['data' =>$user]);
-//     }
-// }
-
-//Method 'firstOrCreate()' mencoba mencari user dengan username 'manager', 
-// jika tidak ditemukan maka membuat dengan data yang diberikan, yaitu 
-// username 'manager' dan nama 'Manager'. kemudian data data tersebut 
-// dikirm ke view menggunakan 'return view()'.
-
-
-//4.
-// class UserController extends Controller
-// {
-//     public function index()
-//     {
-//         $user = UserModel::firstOrCreate(
-//             [
-//                 'username' => 'manager22',
-//                 'nama' => 'Manager Dua Dua',
-//                 'password' => Hash::make('12345'),
-//                 'level_id' => 2
-//             ],
-//         );
-//         return view('user', ['data' =>$user]);
-//     }
-// }
-
-//muncul error karena belum melengkapi 
-// protected $fillable = ['level_id', 'username', 'nama']; dengan 
-// password didalamnya, sedangkan di phpMyAdmin tetap bisa dieksekusi
-
-
-//6.
-// class UserController extends Controller
-// {
-//     public function index()
-//     {
-//         $user = UserModel::firstOrNew(
-//             [
-//                 'username' => 'manager',
-//                 'nama' => 'Manager',
-//             ],
-//         );
-//         return view('user', ['data' =>$user]);
-//     }
-// }
-
-//Method 'firstOrNew()' mencoba mengambil record pertama yang cocok dengan 
-// atribut yang diberikan, jika ditemukan, maka akan dicetak. Jika tidak 
-// ditemukan maka akan membuat instance baru, tetapi tidak akan 
-// menyimpannya di database.
-
-//8.
-// class UserController extends Controller
-// {
-//     public function index()
-//     {
-//         $user = UserModel::firstOrNew(
-//             [
-//                 'username' => 'manager33',
-//                 'nama' => 'Manager Tiga Tiga',
+//                 'username' => 'manager44',
+//                 'nama' => 'Manager44',
 //                 'password' => Hash::make('12345'),
 //                 'level_id' => 2,
 //             ],
 //         );
-//         return view('user', ['data' =>$user]);
+//         $user->username = 'manager45'; //mengubah atribut
+
+//         $user->isDirty(); //true
+//         $user->isDirty('username'); //true
+//         $user->isDirty('nama'); //false
+//         $user->isDirty(['nama', 'username']); //true
+
+//         $user->isClean(); //false
+//         $user->isClean('username'); //false
+//         $user->isClean('nama'); //true
+//         $user->isClean(['nama', 'username']); //false
+
+//         $user->save();
+
+//         $user->isDirty(); //false
+//         $user->isClean(); //true
+//         dd($user->isDirty());
 //     }
 // }
 
-/*
-Pada tampilan browser data yang diisikan akan tetap muncul tetapi pada 
-bagian ID tidak muncul karena tidak tersimpan di database, sedangkan pada
-phpMyAdmin tidak muncul/tersimpan karena kita menggunakan firstOrNew().
-*/
+//Pada browser muncul false // app\Http\Controllers\UserController.php:39 
+// yang disebabkan penggunaan kode 'dd($user->isDirty());'. penggunaan kode
+// ini mencetak nilai kembalian dari isDirty() menjadi 'false' karena tidak  
+// ada perubahan pada model (karena perubahan telah disimpan dan model menjadi
+// kembali ke keadaan awal pada $user->save();)
 
-//10.
+
+//4.
 class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::firstOrNew(
+        $user = UserModel::create(
             [
-                'username' => 'manager33',
-                'nama' => 'Manager Tiga Tiga',
+                'username' => 'manager11',
+                'nama' => 'Manager11',
                 'password' => Hash::make('12345'),
                 'level_id' => 2,
             ],
         );
+        
+        $user->username = 'manager12'; //mengubah atribut
+
         $user->save();
 
-        return view('user', ['data' =>$user]);
+        $user->wasChanged(); //true
+        $user->wasChanged('username'); //true
+        $user->wasChanged(['username', 'level_id']); //true
+        $user->wasChanged('nama'); //false
+        $user->wasChanged(['nama', 'username']); //true
+        dd($user->wasChanged(['nama', 'username'])); //true
     }
 }
 
-//Pada browser muncul karena nomor ID karena atribut yang diisikan 
-// tersimpan pada database walaupun menggunakan firstOrNew() karena telah
-// menambahkan $user->save();.
-// Pada phpMyAdmin muncul baris baru karena atribut tersimpan di database.
+/*
+Pada browser mucul true karena diatara atribut username dan nama telah
+terjadi perubahan yaitu username, dari 'manager11' menjadi 'manager12'
+*/
+
