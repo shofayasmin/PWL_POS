470e570191ed71b44ee98b3272efae533533540d
shofa yasmin fauziah <yasminshofa.sy@gmail.com>
2024-06-15 23:15:26 +0700

    JS10-P3-Create a RESTful API Logout

---
+++ app/Http/Controllers/Api/LogoutController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+
+class LogoutController extends Controller
+{
+    public function __invoke(Request $request)
+    {
+        $removeToken = JWTAuth::invalidate(JWTAuth::getToken());
+
+        if ($removeToken) {
+            return response()->json([
+                'succes' => true,
+                'message' => 'Logout Berhasil!'
+            ]);
+        }
+    }
+}
