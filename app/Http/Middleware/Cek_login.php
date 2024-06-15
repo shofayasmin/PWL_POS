fc525e44733d1b42918bb7ba75958626a9fceb62
shofa yasmin fauziah <yasminshofa.sy@gmail.com>
2024-06-15 21:48:45 +0700

    jobhsheet 9

---
+++ app/Http/Middleware/Cek_login.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class Cek_login
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        //cek sudah login atau belum. jika belum kembali ke halaman login
+        if(!Auth::check()){
+            return redirect('login');
+        }
+        //simpapn data user pada variable user
+        $user = Auth::user();
+
+        //jika user memiliki level sesuai pada kolom pada lanjutkan request
+        if($user->level_id == $roles){
+            return $next($request);
+        }
+
+        //jika tidak memiliki akses maka kembalikan ke halaman login
+        return redirect('login')->with('error', 'Maaf anda tidak memiliki akses');
+        
+    }
+}
